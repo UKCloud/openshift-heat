e6d08855cdd5f8bc3fe173a8a2f7c963fd13d164
Andy Watkins <awatkins@ukcloud.com>
2017-11-20 15:01:57 +0000

    Scripts to increase number of worker nodes

---
+++ upscale.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -z $(grep worker_scale environment.yaml) ]] ; then
+    echo "Edit environment.yaml and specify the new number of workers in 'worker_scale', then uncomment it."
+    echo "  # worker_scale: 3" >> environment.yaml
+    exit
+fi
+
+echo -e "Please provide a password for the OpenStack tenancy OpenShift will be deployed to..."
+read -s openshift_openstack_password
+
+echo "UPSCALING YOUR OPENSHIFT CLOUD..."
+openstack stack update -f yaml -t openshift.yaml openshift \
+    -e rhel_reg_creds.yaml \
+    -e environment.yaml \
+    --parameter time="$(date)" \
+    --parameter os_auth_url=$OS_AUTH_URL \
+    --parameter os_tenant_id=$OS_TENANT_ID \
+    --parameter os_tenant_name=$OS_TENANT_NAME \
+    --parameter os_region=$OS_REGION_NAME \
+    --parameter openshift_openstack_password=$openshift_openstack_password \
+    --wait
+
