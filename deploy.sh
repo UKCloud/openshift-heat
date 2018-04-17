9ac50360e433bba5ea67a5b853eb2f05aea6bd30
Steve Mulholland <stevemul@users.noreply.github.com>
2018-04-17 15:19:25 +0100

    Feature/satellite (#44)
    *adding satellite config

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ function determineFloatingIpMethod() {
     sed -i -e "s///" openshift.yaml
   fi
 }
-
+ 
 function deployHeatStack() {
   openstack stack create -f yaml -t openshift.yaml openshift-$OS_TENANT_NAME \
     -e rhel_reg_creds.yaml \
