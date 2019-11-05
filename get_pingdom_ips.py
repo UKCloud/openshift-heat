1bb4a60b559e04c14c3a29d77b16c223e7e5dcb1
Gareth Ellner <gellner@ukcloud.com>
2019-11-05 14:32:06 +0000

    New python script which parses pingdom probe RSS to find current EU servers. Output can be appended to environment.yaml

---
+++ get_pingdom_ips.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Script to retrieve list of EU pingdom servers
+
+xmltodict neeeds to be installed with pip3
+ pip3 install xmltodict
+
+
+Predeployment, output is in the correct format to do:
+ ./get_pingdom_ips.py >> environment.yaml
+"""
+
+import xmltodict
+import requests
+
+pingdomrss = requests.get('https://my.pingdom.com/probes/feed')
+
+datadict = xmltodict.parse(pingdomrss.text)
+
+print("  monitoring_allowed_sources: [", end=" ")
+first = True
+
+for probe in datadict['rss']['channel']['item']:
+    if probe['pingdom:region'] == "EU":
+        if first == True:
+            print('\"' + probe['pingdom:ip'] + '/32\"', end="")
+            first = False
+        else:
+            print(", ", end="")
+            print('\"' +probe['pingdom:ip'] + '/32\"', end="")
+print(" ]")
+
+
+
