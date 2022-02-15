d0f6333dd59cfca2884c4ce4984d13636b324b01
benbacon <bbacon@ukcloud.com>
2022-02-15 12:19:46 +0000

    Remove Python unsafe load error and remove search for System Type in RH subscription

--- files/install_config_agent_yum.sh
+++ files/install_config_agent_yum.sh
@@ -38,8 +38,8 @@ then
 fi
 
 # determine pool ID's for red hat subscriptions
-openstackPoolId=$(subscription-manager list --available --matches='Red Hat OpenStack Platform for Service Providers' | awk '/System Type:\s*Physical/' RS='\n\n' | awk '/Pool ID/ {print $NF}')
-openshiftPoolId=$(subscription-manager list --available --matches='Red Hat OpenShift Container Platform for Certified Cloud and Service Providers' | awk '/System Type:\s*Physical/' RS='\n\n' | awk '/Pool ID/ {print $NF}')
+openstackPoolId=$(subscription-manager list --available --matches='Red Hat OpenStack Platform for Service Providers' | awk '/Pool ID/ {print $NF}')
+openshiftPoolId=$(subscription-manager list --available --matches='Red Hat OpenShift Container Platform for Certified Cloud and Service Providers' | awk '/Pool ID/ {print $NF}')
 
 # setup repos & install software packages
 retry subscription-manager attach --pool=$openstackPoolId
